bb72e28b754453eac528cfe3759d5de5f687829b
Semi <34294350+semi921@users.noreply.github.com>
2019-12-16 21:20:29 +1100

    Configured mock data in db.json. Use "npm run dev:server" to access mock data.

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,10 +1,18 @@
 import Taro, { useState } from "@tarojs/taro";
 import { View } from "@tarojs/components";
-import { AtTabBar } from "taro-ui";
+import { AtList, AtListItem } from "taro-ui";
 import "./index.scss";
 
 //navBar Component
 export default class Index extends Taro.Component {
+
+  config = {
+    navigationBarTitleText: '猫本书友'
+  }
+
+  state = {
+    books: []
+  }
 
   constructor(props){
     super(props);
@@ -13,9 +21,23 @@ export default class Index extends Taro.Component {
     }
   }
 
+  async componentWillMount() {
+    const response = await Taro.request({
+      url: `${ API_DB }/books`
+    })
+    console.log(response)
+
+    this.setState({
+      books: response.data
+    })
+
+  }
+
   render(){
+    const { books } = this.state
+
     return(
-      <View>
+      <View className='index'>
         Home
       </View>
     )
