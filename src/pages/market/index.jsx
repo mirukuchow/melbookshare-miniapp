2b47d206f19cfd20091717b1521b847c00291b72
Semi <34294350+semi921@users.noreply.github.com>
2019-12-23 23:08:49 +1100

    Moved index page to market

--- src/pages/market/index.jsx
+++ src/pages/market/index.jsx
@@ -1,14 +1,137 @@
 import Taro, { Component } from "@tarojs/taro";
-import { View } from "@tarojs/components";
+import { View, Text } from "@tarojs/components";
+import { AtPagination } from "taro-ui";
+import SearchBar from "../../components/searchbar";
+import BookList from "../../components/booklist";
+import Placeholder from "../../components/placeholder";
+import fetchData from "../../utilities/fetch-data";
 
-class UserProfile extends Component {
+//navBar Component
+export default class Index extends Component {
+
+  constructor(){
+    this.fetchData = fetchData
+  }
+
+  state = {
+    books: [],
+    placeholder: true,
+    total: 0,
+    pageSize: 10,
+    current: 1,
+    serviceError: false,
+    search: ''
+  };
+
+  async componentWillMount() {
+    this.fetchData({
+      resource: 'books',
+      page: this.state.current,
+      pageSize: this.state.pageSize,
+      success: this.fetchDataSuccess.bind(this),
+      fail: this.fetchDataFail.bind(this)
+    })
+  }
+
+  fetchDataSuccess(response) {
+    const { data, header } = response
+    this.setState({
+      books: data,
+      placeholder: false,
+      total: header['X-Total-Count']
+    })
+  }
+
+  fetchDataFail(response) {
+    this.setState({
+      serviceError: true,
+      placeholder: false
+    })
+  }
+
+  onPageChange({ current }) {
+    this.setState({
+      current,
+      placeholder: true
+    }, () => {
+      this.fetchData({
+        resource: 'books',
+        page: this.state.current,
+        pageSize: this.state.pageSize,
+        success: this.fetchDataSuccess.bind(this),
+        fail: this.fetchDataFail.bind(this)
+      })
+    })
+  }
+
+  search(value) {
+    this.fetchData({
+      resource: 'books',
+      search: value,
+      page: this.state.current,
+      pageSize: this.state.pageSize,
+      success: this.fetchDataSuccess.bind(this),
+      fail: this.fetchDataFail.bind(this)
+    })
+  }
+
+  onChangeSearchBar(value) {
+    this.setState({
+      search: value
+    }, () => {
+      console.log(value)
+      this.search(value)
+    })
+  }
+  //When clicking search button
+  onActionClickSearchBar() {
+    this.search(this.state.search)
+  }
+  //When pressing enter button
+  onConfirmSearchBar() {
+    //this.search(this.state.search)
+  }
+
   config = {
-    navigationBarTitleText: "UserProfile"
+    navigationBarTitleText: '猫本书友'
   };
 
   render() {
-    return <View>User Profile</View>;
+
+    const { books, placeholder, total, pageSize, current, serviceError } = this.state
+    const page = (
+      <View className="result__bg">
+        <SearchBar
+          value={this.state.search}
+          onChange={this.onChangeSearchBar.bind(this)}
+          onActionClick={this.onActionClickSearchBar.bind(this)}
+          onConfirm={this.onConfirmSearchBar.bind(this)}
+        />
+        <Placeholder className='m-3' quantity='10' show={placeholder} />
+        {!placeholder && <BookList data={books} />}
+        <AtPagination
+          icon
+          total={parseInt(total)}
+          pageSize={pageSize}
+          current={current}
+          className='my-4'
+          onPageChange={this.onPageChange.bind(this)}
+        />
+      </View>
+    )
+    const errorPage = (
+      <Text className='page-demo'>
+        Kotakun找不到了！！！\n\n\n\n\n\n
+        <Text className='text-xs'>
+          运行npm run dev:server了吗？
+        </Text>
+      </Text>
+
+    )
+    return (
+      <View>
+        {serviceError ? errorPage : page}
+      </View>
+    );
   }
 }
-
-export default UserProfile;
