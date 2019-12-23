c63f5f4a2df90b16eae1cc5b3a172087f9c5567c
Semi <34294350+semi921@users.noreply.github.com>
2019-12-23 20:44:08 +1100

    Recreated mock data based on current Prisma configuration.

--- src/components/booklist-item/index.jsx
+++ src/components/booklist-item/index.jsx
@@ -7,7 +7,7 @@ const BookListItem = ({ data: book }) => (
   <View className="card__item my-2 mx-3">
     <Image className="card__item-image" src={book.image.src} mode="aspectFit" />
     <View className="card__item-content">
-      <View className="card__item-content-header mb-1">{book.name}</View>
+      <View className="card__item-content-header mb-1">{book.title}</View>
       <View className="card__item-content-subtitle text-muted mb-5">
         {book.author}
       </View>
@@ -15,7 +15,7 @@ const BookListItem = ({ data: book }) => (
         <View className="card__item-content-footer-sm">
           <Text className="text-xs">原价\n</Text>
           <Text className="mr-3 text-muted text-through">
-            {"￥" + book.originalPrice}
+            {"￥" + book.availableBooks}
           </Text>
         </View>
         <View className="card__item-content-footer-lg">
