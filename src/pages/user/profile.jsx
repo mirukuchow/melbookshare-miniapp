46c8fbf90a287c1480694227c88bdc43ec48d11e
molly <molly.zhou@rea-group.com>
2019-12-26 21:14:34 +1100

    first graphql connection

--- src/pages/user/profile.jsx
+++ src/pages/user/profile.jsx
@@ -1,7 +1,37 @@
 import Taro, { Component } from "@tarojs/taro";
 import { View } from "@tarojs/components";
+import { gql } from "apollo-boost";
+
+import client from "../../utilities/graphql";
 
 class UserProfile extends Component {
+  componentDidMount() {
+    client
+      .query({
+        query: gql`
+          query getCopiesByOwnerId($id: ID!) {
+            getCopiesByOwnerId(id: $id) {
+              id
+              book {
+                title
+                image
+                author
+                rating
+              }
+            }
+          }
+        `,
+        variables: { id: "123" }
+      })
+      .then(data =>
+        console.log("data", data)
+      )
+      .catch(error => {
+        console.error(error.graphQLErrors[0].message);
+        console.error(error.message);
+        console.error(error.networkError);
+      });
+  }
   config = {
     navigationBarTitleText: "UserProfile"
   };
