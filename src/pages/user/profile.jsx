0371b5206b7e376f5471a3bdaf0852b82a2b1e63
molly <molly.zhou@rea-group.com>
2019-12-29 23:52:12 +1100

    fetch user openid as identifier

--- src/pages/user/profile.jsx
+++ src/pages/user/profile.jsx
@@ -1,5 +1,5 @@
 import Taro, { Component } from "@tarojs/taro";
-import { View } from "@tarojs/components";
+import { View, Button } from "@tarojs/components";
 import { gql } from "apollo-boost";
 
 import client from "../../utilities/graphql";
@@ -36,8 +36,24 @@ class UserProfile extends Component {
     navigationBarTitleText: "UserProfile"
   };
 
+  wxLogin = () => {
+    Taro.login({
+      async success (res) {
+        if (res.code) {
+          console.log("res.code", res.code);
+          const cred = await Taro.request({
+            url: `https://api.weixin.qq.com/sns/jscode2session?appid=wx3dfc2961862ee7d2&secret=5360dd86b5bed0515519b090d49a22f3&js_code=${res.code}&grant_type=authorization_code`,
+          })
+          console.log("openid", cred.data.openid); // TODO: this open id will be used as user id
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+  }
+
   render() {
-    return <View>User Profile </View>;
+    return <View>User Profile<Button onClick={this.wxLogin}>Login</Button></View>;
   }
 }
 
