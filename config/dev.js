49842e6fb80096de8ab8a7a39f3e52c7a98cbfa5
Semi <34294350+semi921@users.noreply.github.com>
2020-02-05 20:48:37 +1100

    Changed DB Address to Milk's GraphQL one

--- config/dev.js
+++ config/dev.js
@@ -3,7 +3,8 @@ module.exports = {
     NODE_ENV: '"development"'
   },
   defineConstants: {
-    API_DB: "http://localhost:3333",
+    // API_DB: "http://localhost:3333",
+    API_DB: "https://peanut-xowb3v9b.pai.tcloudbase.com/",
     API_DOUBAN: "https://douban-api.now.sh/v2/book"
   },
   weapp: {},
